fba3e23cc257134b63d19341a6f0b637ea59a086
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2021-09-23 22:11:32 +0530

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,97 @@
+package veg_foodie;
+public class Menu 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("\t    BREAKFAST");
+		System.out.println("Sandwich and Ketchup           45");
+		System.out.println("Aloo Paratha                   50");
+		System.out.println("Dosa Chutney                   50");
+		System.out.println("Idly Sambhar                   40");
+		System.out.println("Poha                           35");
+		System.out.println();
+		
+		System.out.println("\t     RICE");
+		System.out.println("Pulao                         145");
+		System.out.println("Fried Rice                    160");
+		System.out.println("Ghee Rice                     135");
+		System.out.println("Biryani                       160");
+		System.out.println("Jeera Rice                    130");
+		System.out.println();
+		
+		System.out.println("\tCHAPATI AND NAAN");
+		System.out.println("Roti and Gobi Curry           150");
+		System.out.println("Naan and Panner Curry         150");
+		System.out.println("Paratha and Achari Sabzi      140");
+		System.out.println("Kulcha and Navaratna Korma    170");
+		System.out.println("Chapati                       120");
+		System.out.println();
+		
+		System.out.println("\t SOUTH INDIAN");
+		System.out.println("Rasam Rice                    100");
+		System.out.println("Tomato Bath                   160");
+		System.out.println("Bisi Bele Bath                135");
+		System.out.println("Sambhar Rice                  160");
+		System.out.println("Curd Rice with Uppinkayi      130");
+		System.out.println();
+		
+		System.out.println("\t  NORTH INDIAN");
+		System.out.println("Aloo ki Tikki                  40");
+		System.out.println("Bhatura                        80");
+		System.out.println("Dal Fry with TAdka             75");
+		System.out.println("Kadhai Pakoda                  50");
+		System.out.println("Shahi Tukra                    80");
+		System.out.println();
+		
+		System.out.println("\t     CHINESE");
+		System.out.println("Schezwan Noodles              180");
+		System.out.println("Gobi Manchurian               100");
+		System.out.println("Fried Rice                    160");
+		System.out.println("Momos                          80");
+		System.out.println("Chowmein                       80");
+		System.out.println();
+		
+		System.out.println("\t     ITALIAN");
+		System.out.println("Brushetta                     100");
+		System.out.println("Four Cheese Pasta             130");
+		System.out.println("Spinach Lasagna Roll-Ups      115");
+		System.out.println("Lighter Baked Macaroni & \nCheese with Spinach & Red     105 \nPeppers");
+		System.out.println("Italiano special pizza        120");
+		System.out.println();
+		
+		System.out.println("\t     THAI");
+		System.out.println("Thai Rice                     120");
+		System.out.println("Thai Tofu Vegetable            70");
+		System.out.println("Phrik nu Chillies              70");
+		System.out.println("Galangal                      120");
+		System.out.println("Thai Chilli Brocolli Salad    110");
+		System.out.println();
+		
+		System.out.println("\t     MEXICAN");
+		System.out.println("Mexican Pasta                 120");
+		System.out.println("Black Bean and Corn           110 \nQuesadillas");
+		System.out.println("Mexican Bean Burgers          100");
+		System.out.println("Chilli Potato Burritos         90");
+		System.out.println("3 Cheese Enchilados           100");
+		System.out.println();
+		
+		System.out.println("\t  SNACKS(Baked)");
+		System.out.println("Cake                          150");
+		System.out.println("Pastries                      100");
+		System.out.println("Biscuit                        90");
+		System.out.println("Puffs                          95");
+		System.out.println("Nachos                         85");
+		System.out.println();
+		
+		System.out.println("\t  SNACKS(Fried)");
+		System.out.println("French Fries                   70");
+		System.out.println("Nuggets                        80");
+		System.out.println("Chips                          40");
+		System.out.println("Samosa                         50");
+		System.out.println("Kachori                        50");
+		System.out.println();
+		
+		System.out.println("DINNER                        150");
+		System.out.println("(Includes 1 bowl rice and \ncooked vegetables, indian \nbread with dal and paneer\nand pudding)");
+	}
+}
